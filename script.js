b49273dc6800eda5b7ac2ceb7d485e426d90752e
Travis Batts <Travisbatts6@gmail.com>
2017-04-14 15:13:15 -0400

    touch ups

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
 $( document ).ready(function(){
 
-//   window.verticalScroller = function($elem) {
-//       var bottom = parseInt($elem.css("bottom"));
-//       var temp = -1 * $('#verticalScroller > div').width();
-//       if(bottom < temp) {
-//           bottom = $('#verticalScroller').width()
-//           $elem.css("bottom", bottom);
-//       }
-//       $elem.animate({ bottom: (parseInt(bottom)-60) }, 600, function () {
-//         window.verticalScroller($(this))
-//       });
-//   }
-//
-//
-//   $(document).ready(function() {
-//       var i = 0;
-//       $("#verticalScroller > div").each(function () {
-//             $(this).css("bottom", i);
-//             i += 60;
-//             window.verticalScroller($(this));
-//       });
-//   });
-//
-// })
-
+  $("button").mouseenter(function () {
+    $(this).css("text-decoration", "line-through");
+    $("button").mouseout(function() {
+      $(this).css("text-decoration", "none");
+    })
+ })
 })
