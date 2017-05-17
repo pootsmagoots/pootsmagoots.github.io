e4fbe6d2714deb9cb30b412cc06fe676961809ba
Travis Batts <Travisbatts6@gmail.com>
2017-05-16 22:56:00 -0400

    complete overhaul

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ $( document ).ready(function(){
       $(this).css("text-decoration", "none");
     })
  })
+ /* just to see boxes in "dev mode" */
+$('.dev').on('click', function(){
+  $('.part').toggleClass('devmode');
+  return false;
+});
 })
