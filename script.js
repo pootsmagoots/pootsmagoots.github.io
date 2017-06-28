514172959dec9110f2c101b450b19118147564c1
Travis Batts <Travisbatts6@gmail.com>
2017-06-28 00:07:55 -0400

    save

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ $( document ).ready(function(){
     })
  })
  /* just to see boxes in "dev mode" */
-$('.dev').on('click', function(){
-  $('.part').toggleClass('devmode');
-  return false;
-});
+// $('.dev').on('click', function(){
+//   $('.part').toggleClass('devmode');
+//   return false;
+// });
 
 })
