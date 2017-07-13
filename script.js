79a133513eae4f59e59d337c9e614526506ab49b
Travis Batts <Travisbatts6@gmail.com>
2017-07-13 16:57:23 -0400

    added explosion effect to logo

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 $( document ).ready(function(){
-
+  
+ // event handler for buttons on navigation bar
   $("button").mouseenter(function () {
     $(this).css("text-decoration", "line-through");
     $("button").mouseout(function() {
       $(this).css("text-decoration", "none");
     })
  })
+  // event handeler for Logo TB
+ $('#travis').on("click", function() {
+   $(this).hide('explode', {pieces: 30 }, 2000);
+    $('#travis').fadeIn( "slow" );
+  });
+
+});
+// // $('.email_header').mouseover(function(){
+// //   $(this).css("background-color", "red");
+// // });
+// $( ".email_header" ).mouseover(function() {
+// $( "#log" ).append( "<div>Handler for .mouseover() called.</div>" );
+// });
+
+ //    $('.thumbnail').hover(function(){
+ //     $(this).trigger('slide');
+ // }, function(){
+ //     $(this).trigger('stoslide');
+ // });
+
+
+ // $('#travis').click(function() {
+ //
+ // });
+
+// });
+ // });
   //   $(".pull-me").click(function(){
   //    $(".panel").slideToggle("slow", 0);
   //  });
@@ -15,5 +43,3 @@ $( document ).ready(function(){
   //  $("pull-me").click(function(){
   //    $(".panel").slideToggle("slow", 0);
   //  });
-
-})
